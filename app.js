9e9b2919d304d90831b589e18342d2838d39ef73
Jeremy Magland <magland@system76-pc.localdomain>
2024-01-25 12:35:05 -0500

    debug

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const TARGET_API = 'http://a03f3783a4f8a4745b529e39c1416ba9-927245827.us-east-1.elb.amazonaws.com'
 
+// Enable CORS
+const allowedOrigins = [
+    'https://spyndle.surge.sh',
+    'http://localhost:3000',
+    'http://localhost:5173'
+]
+console.log('---------------------------- testA')
+app.use(cors({
+    origin: function (origin, callback) {
+        console.log('---------------- test1', origin)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            console.log('---------------- test2', origin)
+            var msg = 'The CORS policy for this site does not ' +
+                      'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        console.log('---------------- test3', origin)
+        return callback(null, true);
+    }
+}));
+
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
 app.all('*', async (req, res) => {
+    console.log('--------------- got request')
     try {
         const targetUrl = TARGET_API + req.originalUrl;
 
